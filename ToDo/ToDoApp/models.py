21c4aba121739edc20b74e14fe45e84ff78d5037
UygarKAYA <uygar.kaya@ozu.edu.tr>
2021-08-16 21:46:11 +0300

    ToDoApp Model is created

--- ToDo/ToDoApp/models.py
+++ ToDo/ToDoApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ToDoApp(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Title')
+    description = models.CharField(max_length=50, verbose_name='Description')
+    isComplated = models.BooleanField(verbose_name='isCompleted')
+
+    def __str__(self):
+        return self.title
